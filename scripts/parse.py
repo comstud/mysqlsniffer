1a7382b5e84cbc15858453636d40237315e8cb5f
Chris Behrens <cbehrens@codestud.com>
2013-09-24 22:43:54 +0000

    Better query cleanup in parse.py
    Also make it accept file on command line

--- scripts/parse.py
+++ scripts/parse.py
@@ -4,14 +4,25 @@
 
 import collections
 import re
+import sys
 
 QUERY_RE = re.compile('^(\d+) (\d+) \((\d+)\): COM_QUERY: (SELECT.*)')
+SELECT_CRAP_RE = re.compile("SELECT .*? FROM")
 STRING_RE = re.compile("'[\w\-\.]+'")
-DATE_RE = re.compile("'\d\d\d\d\-\d\d\-\d\d \d\d:\d\d:\d\d\.\d+'")
+DATE_RE = re.compile("'\d\d\d\d\-\d\d\-\d\d \d\d:\d\d:\d\d(\.\d+)*'")
 INT_RE = re.compile("= \d+")
+MAC_RE = re.compile("mac = '(\w\w:){5}\w\w'")
 # Must be last
 IN_RE = re.compile("IN \(.*'<argument>'\)")
-INST_Q_RE = re.compile("SELECT anon_1.instances_created_at AS anon_1_instances_created_at.*FROM..SELECT..*FROM instances")
+MULTI_JOIN_RE = re.compile("LEFT OUTER JOIN (\S+)")
+JOIN_RE = re.compile("LEFT OUTER JOIN.*(LIMIT|WHERE|\)|$)")
+
+def parse_joins(q_str):
+    m = MULTI_JOIN_RE.findall(q_str)
+    if not m:
+        return q_str
+    join_str = "JOIN %s" % str(sorted(m))
+    return JOIN_RE.sub(join_str, q_str)
 
 def parse_file(queries, fname):
     with open(fname, 'r') as f:
@@ -20,11 +31,15 @@ def parse_file(queries, fname):
             if not m:
                 continue
             resp_sz, rows, query_sz, query = m.groups()
+
+            # Convert "SELECT <crap> FROM"
+            query = SELECT_CRAP_RE.sub("SELECT <crap> FROM", query)
             query = STRING_RE.sub("'<argument>'", query)
             query = DATE_RE.sub("'<date>'", query)
             query = INT_RE.sub("= <number>", query)
+            query = MAC_RE.sub("= <macaddr>", query)
             query = IN_RE.sub("IN (<LIST>)", query)
-            query = INST_Q_RE.sub("SELECT <instance_shit> FROM (SELECT <more_instance_shit> FROM instances", query)
+            query = parse_joins(query)
             queries[query]['queries'] += 1
             queries[query]['resp_sz'] += int(resp_sz)
             queries[query]['rows'] += int(rows)
@@ -32,7 +47,7 @@ def parse_file(queries, fname):
 
 queries = collections.defaultdict(lambda: collections.defaultdict(int))
 
-parse_file(queries, 'queries.txt')
+parse_file(queries, sys.argv[1])
 
 for query in queries.keys():
     qryinfo = queries[query]
